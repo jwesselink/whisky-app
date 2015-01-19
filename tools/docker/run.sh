2a16d494c5f5ca0cbb0b93d186584f0c2f7694cf
Jasper Wesselink <jwesselink@gmail.com>
2015-01-19 23:08:30 +0100

    Link docker container to db container

--- tools/docker/run.sh
+++ tools/docker/run.sh
@@ -2,4 +2,4 @@
 
 docker stop whisky-app
 docker rm whisky-app
-docker run -p 8888:8080 --name whisky-app -d repsaj/whisky-app
+docker run -p 8888:8080 --link mongodb:db --name whisky-app -d repsaj/whisky-app
